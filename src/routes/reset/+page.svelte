785b8b053c9ccb208449ccb35ba8a70381773644
Dean Fogarty <angrytongan@gmail.com>
2023-01-22 07:06:36 +1000

    Add reset example.

---
+++ src/routes/reset/+page.svelte
@@ -0,0 +1,5 @@
+<form method="POST">
+    <input type="text" name="first" value="hello" placeholder="first" />
+    <input type="text" name="last" value="world" placeholder="last" />
+    <input type="reset" />
+</form>
